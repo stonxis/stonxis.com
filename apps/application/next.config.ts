0332de1d759d4f75e1dcc69be2f5cca730967f4a
Sqlitt <118855875+sqlitt@users.noreply.github.com>
2025-03-13 12:46:35 -0300

    hidde middleware

--- apps/application/next.config.ts
+++ apps/application/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    EMAIL_SERVER_PORT: process.env.EMAIL_SERVER_PORT,
+    EMAIL_SERVER_HOST: process.env.EMAIL_SERVER_HOST,
+    EMAIL_SERVER_USER: process.env.EMAIL_SERVER_USER,
+    EMAIL_SERVER_PASSWORD: process.env.EMAIL_SERVER_PASSWORD,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+  }
 };
 
 export default nextConfig;
