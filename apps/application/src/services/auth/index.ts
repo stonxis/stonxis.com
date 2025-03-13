703eac51ae67f61aa48772865eab21a7e5b7fc25
Sqlitt <118855875+sqlitt@users.noreply.github.com>
2025-03-13 12:38:04 -0300

    hidde middleware

--- apps/application/src/services/auth/index.ts
+++ apps/application/src/services/auth/index.ts
@@ -27,7 +27,7 @@ export const {
                 port: port,
                 auth: {
                     user: process.env.EMAIL_SERVER_USER,
-                    pass: process.env.RESEND_API_KEY,
+                    pass: process.env.EMAIL_SERVER_PASSWORD,
                 },
             },
             from: process.env.EMAIL_FROM,
