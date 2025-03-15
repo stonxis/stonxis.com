308b4d745e4f86cc3cde25f5f1a7f62d7b9a7c76
Sqlitt <118855875+sqlitt@users.noreply.github.com>
2025-03-15 00:36:04 -0300

    resolve theme and policie of cookies

--- apps/application/src/components/notification-originui.tsx
+++ apps/application/src/components/notification-originui.tsx
@@ -106,7 +106,7 @@ export default function NotificationsOriginUI() {
           )}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-80 p-1">
+      <PopoverContent className="w-80 p-1 md:mr-15 mr-9">
         <div className="flex items-baseline justify-between gap-4 px-3 py-2">
           <div className="text-sm font-semibold">Notifications</div>
           {unreadCount > 0 && (
